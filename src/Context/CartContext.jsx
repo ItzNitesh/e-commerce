2077f2ddc2e4eea74a19208c5056a065c743ee56
Nitesh Gupta <niteshgupta6678@gmail.com>
2026-06-23 21:30:09 +0530

    Added cart functionality with Context API

--- src/Context/CartContext.jsx
+++ src/Context/CartContext.jsx
@@ -1,9 +1,16 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 export const CartContext = createContext();
 
 export const CartProvider = ({ children }) => {
-  const [cartItem, setCartItem] = useState([]);
+  const [cartItem, setCartItem] = useState(() => {
+    const saveCart = localStorage.getItem("cart");
+    return saveCart ? JSON.parse(saveCart) : []
+  })
+
+  useEffect(()=>{
+    localStorage.setItem("cart", JSON.stringify(cartItem))
+  },[cartItem]);
 
   return (
     <CartContext.Provider
